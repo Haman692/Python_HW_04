987d738d487e06eb9e37f55016426689a19b9f6a
Haman692 <Haman692@yandex.ru>
2023-02-09 14:33:03 +0700

    4

--- task_04.py
+++ task_04.py
@@ -3,14 +3,14 @@ import random
 
 print('Напишите степень ')
 deg = int(input())
-numbers = []
-randomNumber = random.randint(1,3)
-if(randomNumber == 1):
-    polynomial = f'{random.randint(1, 100)} * x^{deg} = 0'
-if(randomNumber == 2):
-    polynomial = f'{random.randint(1, 100)} * x^{deg} + {random.randint(1, 100)} * x  + {random.randint(1, 100)} = 0'
-if(randomNumber == 3):
-    polynomial = f'{random.randint(1, 100)} * x^{deg} + {random.randint(1, 100)} * x^{deg - 1}  + x x^{-deg}= 0'
+numbers = [random.randint(0,100) for i in range(random.randint(1,3))]
+if(len(numbers) == 1):
+    polynomial = f'{numbers[0]} * x^{deg} = 0'
+if(len(numbers) == 3):
+    polynomial = f'{numbers[0]} * x^{deg} + {numbers[1]} * x  + {numbers[2]} = 0'
+if(len(numbers) == 2):
+    polynomial = f'{numbers[0]} * x^{deg} + {numbers[1]} * x^{deg - 1}  + x x^{-deg}= 0'
 file = open("Polynomial.txt", "w")
 file.write(polynomial)
 file.close
+print(numbers)
